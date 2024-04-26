198bc512739d371959740604fdbbc7816aed1d79
moumitacloud08 <moumitacloud08@gmail.com>
2024-04-26 12:31:05 -0700

    Spring data jpa configure

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Dynamic port return
 Go to Run Configuration -> Rename the already created application as currency-exchange-service8000(It will run on the port as mentioned in propertie file)
 Create a duplicate application and rename currency-exchange-service8001 (it will run on the port mentioned in Vm arguments)
 -Dserver.port=8001
+
+h2Db console URL:- http://localhost:8000/h2-console
--- src/main/java/com/example/microservices/currencyexchangeservice/ExchangeValue.java
+++ src/main/java/com/example/microservices/currencyexchangeservice/ExchangeValue.java
@@ -2,10 +2,22 @@ package com.example.microservices.currencyexchangeservice;
 
 import java.math.BigDecimal;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name = "exchange_value")
 public class ExchangeValue {
+	@Id
 	private Long id;
+	@Column(name="currency_from")
 	private String from;
+	@Column(name="currency_to")
 	private String to;
+	@Column(name="conversion_multiple")
 	private BigDecimal conversionMultiple;
 	private int port;
 
