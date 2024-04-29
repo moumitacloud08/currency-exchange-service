b413cc6b2e2f88309b269a67d2da1c7159e918be
moumitacloud08 <moumitacloud08@gmail.com>
2024-04-29 10:17:10 -0700

    Configure below features
        @Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
    	@CircuitBreaker(name="default", fallbackMethod = "hardcodedResponse")
    	@RateLimiter(name = "default")
    	@Bulkhead(name = "sample-api")

--- src/main/java/com/example/microservices/currencyexchangeservice/CircuitBreakerController.java
+++ src/main/java/com/example/microservices/currencyexchangeservice/CircuitBreakerController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 
 @RestController
@@ -15,12 +18,26 @@ public class CircuitBreakerController {
 	private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
 
 	@GetMapping("sample-api")
-	@Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
+	//@Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
+	//@CircuitBreaker(name="default", fallbackMethod = "hardcodedResponse")
+	//@RateLimiter(name = "default")
+	//10s => 1000 calls to the sample api
+	@Bulkhead(name = "sample-api")
 	public String sampleApi() {
 		logger.info("Sample Api call recieved");
-		ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url",
-				String.class);
-		return forEntity.getBody();
+		
+		/*
+		 * commenting to check @RateLimiter because it returns when request is
+		 * successful and we hit more requests withing the mentioned time then it will
+		 * return error
+		 */
+		/*
+		 * ResponseEntity<String> forEntity = new
+		 * RestTemplate().getForEntity("http://localhost:8080/some-dummy-url",
+		 * String.class); return forEntity.getBody();
+		 */
+		
+		return "sample-api";
 	}
 
 	public String hardcodedResponse(Exception ex) {
