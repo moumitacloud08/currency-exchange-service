fe3da9aa07bb56c3a1ed63bb0c0ea0d7ae6db38a
moumitacloud08 <moumitacloud08@gmail.com>
2024-04-29 06:36:55 -0700

    configure sample fallbackMethod

--- src/main/java/com/example/microservices/currencyexchangeservice/CircuitBreakerController.java
+++ src/main/java/com/example/microservices/currencyexchangeservice/CircuitBreakerController.java
@@ -13,13 +13,17 @@ import io.github.resilience4j.retry.annotation.Retry;
 public class CircuitBreakerController {
 
 	private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
-	
+
 	@GetMapping("sample-api")
-	@Retry(name = "sample-api")
+	@Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
 	public String sampleApi() {
 		logger.info("Sample Api call recieved");
-		ResponseEntity<String> forEntity=  new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url", String.class);
+		ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url",
+				String.class);
 		return forEntity.getBody();
 	}
 
+	public String hardcodedResponse(Exception ex) {
+		return "fallback-response";
+	}
 }
