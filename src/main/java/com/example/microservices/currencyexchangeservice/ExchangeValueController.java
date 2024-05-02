b8f9a4459f97b27e465223fbf8007a5b6e08f93b
moumitacloud08 <moumitacloud08@gmail.com>
2024-05-01 22:36:29 -0700

    pom modified for image build

--- src/main/java/com/example/microservices/currencyexchangeservice/ExchangeValueController.java
+++ src/main/java/com/example/microservices/currencyexchangeservice/ExchangeValueController.java
@@ -21,6 +21,8 @@ public class ExchangeValueController {
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public ExchangeValue exchangeService(@PathVariable String from, @PathVariable String to) {
 		
+		//2024-05-01T09:13:40.876-07:00  INFO [currency-exchange-service,75be202d6e79245576070c162661d9c7,64a8ae857fbd3d56]  6304 --- [currency-exchange-service] [nio-8000-exec-1] [75be202d6e79245576070c162661d9c7-64a8ae857fbd3d56] c.e.m.c.ExchangeValueController          : exchangeService called with EUR to INR
+
 		logger.info("exchangeService called with {} to {}", from,to);
 		ExchangeValue exchangeValue = repository.findByFromAndTo(from, to);
 		
